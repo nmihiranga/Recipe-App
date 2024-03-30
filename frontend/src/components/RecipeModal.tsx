b253be687aa07376cceefd3daae0e96ef20484ef
nmihiranga <mihiranganayana@gmail.com>
2024-03-31 05:26:53 +0530

    new commit

---
+++ frontend/src/components/RecipeModal.tsx
@@ -0,0 +1,12 @@
+const RecipeModal = ()=> {
+  return (
+    <>
+      <div className="overlay"></div>
+      <div className="modal">
+        <div className="modal-content">
+          <div className="modal-header"></div>
+        </div>
+      </div>
+    </>
+  );
+}
